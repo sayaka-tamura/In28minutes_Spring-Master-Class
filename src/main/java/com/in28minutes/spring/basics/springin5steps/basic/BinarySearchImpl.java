7fbf09b491fca7c7430ec0c95bf38a83c4f1e441
sayaka-tamura <arccycle@gmail.com>
2019-05-13 18:40:07 +0900

    Add @PostConstruct and @PreDestroy to SpringIn5StepsComponentApplication

--- src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -6,6 +6,11 @@
  */
 package com.in28minutes.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -15,8 +20,10 @@ import org.springframework.stereotype.Component;
 // Java Bean : a software component that has been designed to be reusable in a variety of different environments. 
 
 @Component // tells Spring "This is a bean that has to manage"
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // Change the default scope (singleton) to prototype
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) // Change the default scope (singleton) to prototype
 public class BinarySearchImpl {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	// tells Spring "that is a dependency"
 	@Autowired
@@ -36,4 +43,14 @@ public class BinarySearchImpl {
 
 		return 3;
 	};
+
+	@PostConstruct // Called after a auto-wired dependency has created
+	public void postConstruct() {
+		logger.info("postConstruct");
+	}
+
+	@PreDestroy // Called after a auto-wired dependency has removed
+	public void preDestroy() {
+		logger.info("preDestory");
+	}
 }
